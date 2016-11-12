a56627d9c7f0134ca03d87693bedf793712bec4f
Martins Sipenko <martins.sipenko@gmail.com>
2016-11-12 23:43:48 +0200

    adminer

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+$app->get('/adminer', 'AdminerController@adminer');
+$app->post('/adminer', 'AdminerController@adminer');
+$app->put('/adminer', 'AdminerController@adminer');
+$app->patch('/adminer', 'AdminerController@adminer');
+$app->options('/adminer', 'AdminerController@adminer');
+$app->delete('/adminer', 'AdminerController@adminer');
+
 $app->get('/quotes', 'QuoteController@getQuote');
 $app->post('/quotes', 'QuoteController@getQuote');
 
