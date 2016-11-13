d3e8c8c72b9bba0445c9dcefa8d67bdb15bc8651
Martins Sipenko <martins.sipenko@gmail.com>
2016-11-13 11:41:42 +0200

    emojiiiiis

--- app/Jobs/SendFirstEmail.php
+++ app/Jobs/SendFirstEmail.php
@@ -42,7 +42,7 @@ class SendFirstEmail extends Job
         $sendResult = $this->email->sendEmail(
             'noreply@bidonme.eu',
             $this->transaction->customer_email,
-            "Welocome to insurance FightClub!",
+            "Welocome to insurance FightClub! 👊💥",
             str_replace(
                 '{{ link }}',
                 "https://www.bidonme.eu/fight.html?id={$this->transaction->id_hash}",
