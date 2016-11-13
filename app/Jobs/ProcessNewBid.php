3d139677ff593a049d031e2d641788199f2aded2
Martins Sipenko <martins.sipenko@gmail.com>
2016-11-13 12:17:30 +0200

    fix

--- app/Jobs/ProcessNewBid.php
+++ app/Jobs/ProcessNewBid.php
@@ -43,7 +43,11 @@ class ProcessNewBid extends Job
         if ($this->bid->round->number > 1) {
             \Log::info('Recurring bid');
 
-            $your_price = $transaction->bids()->where('insurer_id', $this->bid->insurer_id)->orderBy('id', 'desc')->get()[1]->offer_price;
+            $your_price = $transaction->bids()
+                ->whereNotNull('offer_price')
+                ->where('insurer_id', $this->bid->insurer_id)
+                ->orderBy('id', 'desc')
+                ->get()[1]->offer_price;
             $your_price = $your_price / 100;
 
             $best_price = $transaction->bids()->whereNotNull('offer_price')->orderBy('offer_price', 'asc')->first()->offer_price;
