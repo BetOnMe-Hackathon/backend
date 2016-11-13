f58aae630c61ae942f48040f948a096fe92c8bc4
Martins Sipenko <martins.sipenko@gmail.com>
2016-11-13 14:45:55 +0200

    fix

--- app/Jobs/ProcessNewBid.php
+++ app/Jobs/ProcessNewBid.php
@@ -71,17 +71,12 @@ class ProcessNewBid extends Job
         $data = str_replace("\n", "<br>\n", json_encode($transaction->data, JSON_PRETTY_PRINT));
 
         $sendResult = $this->email->sendEmail(
-            'bidonme@bidonme.eu',
+            $from,
             $to,
             'Insurance quote request!',
             "Transaction: {$transaction->id_hash} <br>Round: {$this->bid->round->number} <br><br>".
             $prev_round_offer.
             "Policy requirements:<br>".$data,
-            null,
-            null,
-            null,
-            null,
-            $from
         );
     }
 }
