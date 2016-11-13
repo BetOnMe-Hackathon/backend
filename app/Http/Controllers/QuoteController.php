0d8c9040c8ca7d0572c7087f37f08a0f5caeee50
Martins Sipenko <martins.sipenko@gmail.com>
2016-11-13 09:50:36 +0200

    stuff

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -45,6 +45,8 @@ class QuoteController extends Controller
         return $rounds;
     }
 
+    // curl 'https://api.bidonme.eu/quotes' -H 'pragma: no-cache' -H 'origin: https://www.bidonme.eu' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.8,lv;q=0.6' -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36' -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8' -H 'accept: */*' -H 'cache-control: no-cache' -H 'authority: api.bidonme.eu' -H 'referer: https://www.bidonme.eu/' --data 'customer_email=msipenko%40kasko.io&bidding_duration=86400&data%5Bproperty_type%5D=apartment&data%5Bconstruction_type%5D=brick&data%5Bproperty_size%5D=0&data%5Bdisaster%5D=9000&data%5Brobbery%5D=10000&data%5Bsecurity%5D=false&data%5Breconstruction%5D=true&data%5Bbuilding_year%5D=false' --compressed
+
     public function getQuote(Request $request)
     {
         $this->validate($request, [
